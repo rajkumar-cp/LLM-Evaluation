2725ade3a7c6bcd98cfab8d3afc98584f402bbd8
chidambaramr <chidambaramr@haufe.com>
2026-03-24 15:11:06 +0100

    Added new test test_context_recall

--- BasicMetricsLLM.py
+++ BasicMetricsLLM.py
@@ -1,4 +1,5 @@
 import os
+from random import sample
 
 import pytest
 import requests
@@ -7,7 +8,7 @@ from langchain_openai import ChatOpenAI
 from openai import OpenAI
 from ragas.llms import llm_factory, LangchainLLMWrapper
 from ragas import SingleTurnSample
-from ragas.metrics import LLMContextPrecisionWithoutReference
+from ragas.metrics import LLMContextPrecisionWithoutReference, LLMContextRecall
 
 
 @pytest.mark.asyncio
@@ -31,4 +32,25 @@ async def test_context_precision_without_reference():
     )
     context_precision = LLMContextPrecisionWithoutReference(llm=wrapped_llm)
     score = await context_precision.single_turn_ascore(sample)
-    print(score)
+    assert  score > 0.9
+
+
+async def test_context_recall():
+    llm_client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+    llm_wrapper = llm_factory('gpt-4o-mini', client=llm_client)
+    question = "How many articles are there in selenium python course?"
+    response = requests.post(url="https://rahulshettyacademy.com/rag-llm/ask", json={
+        "question": question,
+        "chat_history": [
+        ]
+    }
+    ).json()
+    retrieved_docs_length = len(response["retrieved_docs"])
+    sample = SingleTurnSample(
+        user_input=question,
+        retrieved_contexts=[response["retrieved_docs"][i]["page_content"] for i in range(retrieved_docs_length)],
+        reference="23"
+    )
+    context_recall = LLMContextRecall(llm=llm_wrapper)
+    score = await context_recall.single_turn_ascore(sample)
+    assert score > 0.9
