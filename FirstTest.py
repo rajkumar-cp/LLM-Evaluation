b7026d6059a79f9ddfa960b561f112769db3a608
chidambaramr <chidambaramr@haufe.com>
2026-03-24 14:26:38 +0100

    Updated logic to generate single turn sample dynamically

--- FirstTest.py
+++ FirstTest.py
@@ -1,6 +1,7 @@
 import os
 
 import pytest
+import requests
 from instructor.cli.jobs import client
 from langchain_openai import ChatOpenAI
 from openai import OpenAI
@@ -11,33 +12,22 @@ from ragas.metrics import LLMContextPrecisionWithoutReference
 
 @pytest.mark.asyncio
 async def test_context_precision_without_reference():
-    client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
+    llm_client = OpenAI(api_key=os.environ["OPENAI_API_KEY"])
     #client = ChatOpenAI(model="gpt-4o-mini", temperature=0)
     #wrapped_llm = LangchainLLMWrapper(client)
-    wrapped_llm = llm_factory('gpt-4o-mini', client=client)
+    wrapped_llm = llm_factory('gpt-4o-mini', client=llm_client)
+    question = "How many articles are there in selenium python course?"
+    response = requests.post(url="https://rahulshettyacademy.com/rag-llm/ask", json={
+        "question": question,
+        "chat_history": [
+        ]
+    }
+    ).json()
+    retrieved_docs_length = len(response["retrieved_docs"])
     sample = SingleTurnSample(
-        user_input="How many articles are there in selenium python course?",
-        response="There are 23 articles in the course",
-        retrieved_contexts=["Complete Understanding on Selenium Python API Methods with real time Scenarios on LIVE "
-                            "Websites\n\"Last but not least\" you can clear any Interview and can Lead Entire "
-                            "Selenium Python Projects from Design Stage\nThis course includes:\n17.5 hours on-demand "
-                            "video\nAssignments\n23 articles\n9 downloadable resources\nAccess on mobile and "
-                            "TV\nCertificate of completion\nRequirements",
-                            "What you'll learn\nAt the end of this course, You will get complete knowledge on Python "
-                            "Automation using Selenium WebDriver\nYou will be able to implement Python Test "
-                            "Automation Frameworks from Scratch with all latest Technlogies\nComplete Understanding "
-                            "of Python Basics with many practise Examples to gain a solid exposure\nYou will be "
-                            "learning Python Unit Test Frameworks like PyTest which will helpful for Unit and "
-                            "Integration Testing,"
-                            "Wish you all the Best! See you all in the course with above topics :)\n\nWho this course "
-                            "is for:\nAutomation Engineers\nSoftware Engineers\nManual testers\nSoftware developers",
-                            "So what makes this course Unique in the Market?\nWe assume that students have no "
-                            "experience in automation / coding and start every topic from scratch and "
-                            "basics.\nExamples are taken from  REAL TIME HOSTED WEB APPLICATIONS  to understand how "
-                            "different components can be automated.\n  Topics includes: \nPython Basics\nPython "
-                            "Programming examples\nPython Data types\nPython OOPS Examples\nSelenium "
-                            "Locators\nSelenium Multi Browser Execution\nPython Selenium API Methods\nAdvanced "
-                            "Selenium User interactions"]
+        user_input=question,
+        response=response["answer"],
+        retrieved_contexts=[response["retrieved_docs"][i]["page_content"] for i in range(retrieved_docs_length)]
     )
     context_precision = LLMContextPrecisionWithoutReference(llm=wrapped_llm)
     score = await context_precision.single_turn_ascore(sample)
